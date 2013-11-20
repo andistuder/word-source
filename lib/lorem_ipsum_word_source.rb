2aeee7fb9d414171ff2f5bc5fa03dd5f864d506a
andistuder <andi@cenatus.org>
2013-11-20 22:11:58 +0000

    add methods run, count and next_word to WordSource

--- lib/lorem_ipsum_word_source.rb
+++ lib/lorem_ipsum_word_source.rb
@@ -1,3 +1,21 @@
 class LoremIpsumWordSource
+  attr_accessor :loaded_words, :seen_words
 
+  def initialize
+    file = File.open("lorem_ipsum.txt")
+    #self.src = file.read
+    @loaded_words = []
+    @loaded_words += file.read.scan(/\w+/)
+    @seen_words = []
+  end
+  def next_word
+    @seen_words << @loaded_words.shift
+    @seen_words.last
+  end
+  def count
+    @seen_words.length
+  end
+  def run
+    !@loaded_words.empty?
+  end
 end
--- spec/lorem_ipsum_word_source_spec.rb
+++ spec/lorem_ipsum_word_source_spec.rb
@@ -1,24 +1,30 @@
 require 'lorem_ipsum_word_source'
 
 describe LoremIpsumWordSource do
+  subject { LoremIpsumWordSource.new }
+
   describe "#initialize" do
     it "should return an instance of LoremIpsumWordSorce" do
-      LoremIpsumWordSource.new.should be_an_instance_of(LoremIpsumWordSource)
+      subject.should be_an_instance_of(LoremIpsumWordSource)
     end
     it "should load lorem_ipsum.txt" do
-      pending
+      #File.should_receive(:open).and_return(:file)
+      subject.loaded_words.length.should == 4946
     end
     it "should be set to the beginning of the loaded string" do
-      pending
+      subject.seen_words.should be_empty
     end
   end
 
   describe "#next_word" do
-    it "should return the next work" do
-      pending
+    it "should return the next word" do
+      subject.next_word.should == 'Lorem'
     end
     it "shift through the loaded string" do
-      pending
+      expected_squence = %w(Lorem ipsum dolor sit amet consectetur)
+      expected_squence.each do |word|
+        subject.next_word.should == word
+      end
     end
   end
 
@@ -50,19 +56,26 @@ describe LoremIpsumWordSource do
 
   describe "#count" do
     it "should return a count of total words seen" do
-      pending
+      n=3
+      n.times do
+        subject.next_word
+      end
+      subject.count.should == 3
     end
   end
 
   describe "#run" do
     context "all loaded words were seen" do
       it "should return false" do
-        pending
+        subject.loaded_words.length.times do
+          subject.next_word
+        end
+        subject.run.should be_false
       end
     end
     context "not all loaded words were seen" do
       it "should return true" do
-        pending
+        subject.run.should be_true
       end
     end
   end
